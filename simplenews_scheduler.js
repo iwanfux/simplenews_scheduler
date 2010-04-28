3152f8aeee57fb5e2d50010fe6a2ff491029045d
Gabor Seljan <sgabe@232117.no-reply.drupal.org>
2010-04-28 15:25:28 +0000

    Minor code formatting changes.

--- simplenews_scheduler.js
+++ simplenews_scheduler.js
@@ -13,10 +13,10 @@ Drupal.behaviors.simplenewsScheduler = function (context) {
     if($(".simplenews-command-send :radio:checked").val() == '3') {
         $('.schedule_info').css({display: "block"});
     } else {
-    	$('.schedule_info').css({display: "none"});
+      $('.schedule_info').css({display: "none"});
     }
   }
-  
+
   // Update scheduler info's display at page load and when a send option is selected.
   $(function() { simplenewsScheduler(); });
   $(".simplenews-command-send").click( function() { simplenewsScheduler(); });
@@ -30,15 +30,15 @@ Drupal.behaviors.simplenewsSchedulerStop = function (context) {
     if($(".simplenews-command-stop :radio:checked").val() == '1') {
         $('#edit-simplenews-scheduler-stop-date-wrapper').css({display: "block"});
     } else {
-    	$('#edit-simplenews-scheduler-stop-date-wrapper').css({display: "none"});
+      $('#edit-simplenews-scheduler-stop-date-wrapper').css({display: "none"});
     }
     if($(".simplenews-command-stop :radio:checked").val() == '2') {
         $('#edit-simplenews-scheduler-stop-edition-wrapper').css({display: "block"});
     } else {
-    	$('#edit-simplenews-scheduler-stop-edition-wrapper').css({display: "none"});
+      $('#edit-simplenews-scheduler-stop-edition-wrapper').css({display: "none"});
     }
   }
-  
+
   // Update scheduler info's display at page load and when a stop option is selected.
   $(function() { simplenewsSchedulerStop(); });
   $(".simplenews-command-stop").click( function() { simplenewsSchedulerStop(); });
