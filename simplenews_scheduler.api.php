e7912117ef0724f1a98bf4e2a6e6bed6e9a89c19
Miro Dietiker <miro.dietiker@md-systems.ch>
2012-01-30 00:39:58 +0100

    codestyle

--- simplenews_scheduler.api.php
+++ simplenews_scheduler.api.php
@@ -8,9 +8,9 @@
 /**
  * Alter the node object that was cloned from the template node before it gets saved.
  *
- * The node is passed as node object and therefore passed by reference. This hook 
- * is for example usefull if you have fields in the template node that contain 
- * information about data that should get rendered dynamically into the edition 
+ * The node is passed as node object and therefore passed by reference. This hook
+ * is for example usefull if you have fields in the template node that contain
+ * information about data that should get rendered dynamically into the edition
  * node depenedent on the current schedule date.
  */
 function hook_simplenews_scheduler_cloned_node_alter($node) {
