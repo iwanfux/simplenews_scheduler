b1111228e8fdc6f27cc8d8d09009c01cf8f3a7ab
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2012-05-04 16:04:40 +0100

    - #1476744 by Berdir: Changed the edition alter hook name and added a parameter for the template scheduler node.

--- simplenews_scheduler.api.php
+++ simplenews_scheduler.api.php
@@ -12,7 +12,12 @@
  * is for example usefull if you have fields in the template node that contain
  * information about data that should get rendered dynamically into the edition
  * node depenedent on the current schedule date.
+ *
+ * @param $edition_node
+ *   The cloned node object based on the scheduler node.
+ * @param $scheduler_node
+ *   The original scheduler node object.
  */
-function hook_simplenews_scheduler_cloned_node_alter($node) {
+function hook_simplenews_scheduler_edition_node_alter($edition_node, $scheduler_node) {
   $node->title = 'Your newsletter from ' . REQUEST_TIME;
 }
